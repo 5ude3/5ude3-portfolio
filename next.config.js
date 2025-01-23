fe616acc3614ca97b508f0e50b180fdb29248e77
5ude3 <dev.sudee@gmail.com>
2025-01-24 01:24:23 +0530

    added .github folder for deployment

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/5ude3-portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+
+module.exports = nextConfig;
